8b5b7551016b935f8c9bebe7f4f35087dc9b4efd
thiago <thiagodeps2@gmail.com>
2025-06-27 18:30:29 -0300

    Primeiro commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='calculadorasimplesthiago',
+    packages=find_packages(),
+    version='0.1.0',
+    description='calculadora para atividade projetos de sistemas',
+    author='Eu',
+    license='MIT',
+)
